e097d56e10368a42356a4fafc568fcb7b3aafc2f
Alejandro Dominguez <adborroto90@gmail.com>
2019-08-11 12:54:18 +0200

    isFeatureEnabled function

--- samples/console/index.js
+++ samples/console/index.js
@@ -3,11 +3,20 @@ var Appconfi = require("../../src/index");
 var manager = new Appconfi({
     env: '[default]',
     app: 'dc97d669-1460-4602-8ae3-2a35b2708df7',
-    apiKey: 'a7822a44-af94-4f0c-9337-7c31f2fe33af'
+    apiKey: 'a7822a44-af94-4f0c-9337-7c31f2fe33af',
+    refreshInterval: 2
 });
 
-manager.getSetting('application.color').then(function(value){
-    console.log(value);
+ manager.startMonitoring();
+
+ manager.isFeatureEnabled('my_awesome_feature').then(function(value){
+    console.log('my_awesome_feature',value);
+}).catch(function(error){
+    console.log(error);
+});
+
+manager.getSetting('my_application_setting').then(function(value){
+    console.log('my_application_setting',value);
 }).catch(function(error){
     console.log(error);
 });
--- src/index.js
+++ src/index.js
@@ -7,8 +7,21 @@ var appconfi = function (config) {
     if (!this || this.constructor !== appconfi) {
         throw new Error('Appconfi must be called as a constructor');
     }
+    if(!config.app) {
+        throw new Error('Invalid configuration. Undefined app');
+    }
+    if(!config.apiKey) {
+        throw new Error('Invalid configuration. Undefined apiKey');
+    }
+
+    if(config.refreshInterval && config.refreshInterval < 2) {
+        throw new Error('Invalid configuration. Min value allowed for refreshInterval is 2 minutes');
+    }
 
     this.config = config;
+    this.monitoring = false;
+    this.configurationVersion = -1;
+
     this.urls = {
         api: 'https://appconfi.com'
     };
@@ -23,7 +36,8 @@ appconfi.prototype._httpCall = function (method, path, callback, optionsToOmit) 
         json: true,
         headers: {
             'Content-Type': 'application/json',
-            'User-Agent': 'Appconfi Node - ' + version
+            'User-Agent': 'Appconfi-NodeJS - ' + version,
+            'X-Appconfi-UA': 'AppConfi-Client NodeJS v' + version
         },
         qsStringifyOptions: { arrayFormat: 'brackets' }
     };
@@ -58,7 +72,6 @@ appconfi.prototype._httpCall = function (method, path, callback, optionsToOmit) 
     });
 };
 
-
 appconfi.prototype._httpGet = function (path) {
     return this._httpCall('GET', this.urls.api + path);
 };
@@ -80,56 +93,106 @@ appconfi.prototype._getVersion = function () {
 };
 
 appconfi.prototype._checkConfiguration = function () {
-
     var self = this;
-
-    //TODO: Check for last version if monitoring
-
-    return this._getVersion().then(function(version){
-        var path = self._buildPath('/api/v1/configurations');
-        return self._httpGet(path).then(function(configuration){
-            self.appConfiguration = configuration;
-        });
-    });
     
+    return new Promise(function (resolve, reject, callback) {
+        self._getVersion().then(function (version) {
 
+            if(self.configurationVersion == version){
+                self.configurationVersion = version;
+                resolve();
+            }
+
+            var path = self._buildPath('/api/v1/configurations');
+            
+            self._httpGet(path).then(function (configuration) {
+                self.configurationVersion = version;
+                self.appConfiguration = configuration;
+                resolve();
+            });
+
+        });  
+    });
 };
-
 
 appconfi.prototype._getSettingFromCache = function (setting) {
 
     var self = this;
     return self.appConfiguration.settings[setting];
+
 };
 
+appconfi.prototype._isFeatureEnabledFromCache = function (toggle) {
+
+    var self = this;
+    return self.appConfiguration.toggles[toggle] == "on";
+    
+};
+
+appconfi.prototype._validCache = function () {
+    if(!this.isMonitoring())
+        return true;
+
+    var now = new Date(),
+        afterCacheIsInvalid = new Date(this.cacheVersion);
+
+    afterCacheIsInvalid.setMinutes(afterCacheIsInvalid.getMinutes() + this.config.refreshInterval);
+
+    return now <= afterCacheIsInvalid;
+
+};
 
 appconfi.prototype.getSetting = function (setting) {
 
     var self = this;
-    
+
     return new Promise(function (resolve, reject, callback) {
 
         //If not monitoring
-        if(!self.isMonitoring() && self.appConfiguration != null){
-            return resolve(self._getSettingFromCache(resolve));
+        if (self.appConfiguration != null && (!self.isMonitoring() || self._validCache())) {
+            return resolve(self._getSettingFromCache(setting));
         }
 
-        //Check for refresh interval
-
         //Check for lastest configuration
-        self._checkConfiguration().then(function(){
+        self._checkConfiguration().then(function () {
             self.cacheVersion = Date.now();
-            return resolve(self._getSettingFromCache(resolve));
+            resolve(self._getSettingFromCache(setting));
         });
     });
 };
 
+appconfi.prototype.isFeatureEnabled = function (toggle) {
+    var self = this;
 
-appconfi.prototype.isFeatureEnabled = function (setting) {
+    return new Promise(function (resolve, reject, callback) {
 
+        //If not monitoring
+        if (self.appConfiguration != null && (!self.isMonitoring() || self._validCache())) {
+            return resolve(self._isFeatureEnabledFromCache(toggle));
+        }
+
+        //Check for lastest configuration
+        self._checkConfiguration().then(function () {
+
+            self.cacheVersion = Date.now();
+            var result = self._isFeatureEnabledFromCache(toggle);
+
+            return resolve(result);
+
+        });
+    });
 };
 
 appconfi.prototype.startMonitoring = function () {
+    
+    if(!this.config.refreshInterval){
+        this.config.refreshInterval = 5;
+    }
+
+    if(this.config.refreshInterval < 2) {
+        throw new Error('Invalid configuration. Min value allowed for refreshInterval is 2 minutes');
+    }
+   
     var self = this;
     self.monitoring = true;
 };
@@ -143,7 +206,5 @@ appconfi.prototype.isMonitoring = function () {
     var self = this;
     return self.monitoring;
 };
-
-
 
 module.exports = appconfi;
